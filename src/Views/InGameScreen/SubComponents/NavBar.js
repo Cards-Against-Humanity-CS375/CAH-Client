4a4dd5894d9bd5ad414e251c4bd7d5fbf73ffac0
Hoang Nam Le <hl662@drexel.edu>
2020-03-11 10:43:18 -0400

    UX improvement

--- src/Views/InGameScreen/SubComponents/NavBar.js
+++ src/Views/InGameScreen/SubComponents/NavBar.js
@@ -8,14 +8,15 @@ const NavBar = (props) => {
     const playerNameItems = props.players.map((player) =>
         <a className="p-2 text-muted" href="#">{player.name}</a>
     )
-
+    // TODO: Find new relevant emojis 
     return (
         <>
             <header className="blog-header py-3">
                 <div className="row flex-nowrap justify-content-between align-items-center">
                     <div className="col-4 pt-1">
-                        <Button variant="outline-secondary">
-                            <Image src="./assets/crown.svg" fluid />
+                        <Button variant="outline-secondary"> 
+                            <div>My score:</div>
+                            <Image src="./assets/crown.svg" fluid/>
                             <Badge variant="warning" className="mx-1">{props.points}</Badge>
                         </Button>
                     </div>
@@ -24,6 +25,7 @@ const NavBar = (props) => {
                     </div>
                     <div className="col-4 pt-1 d-flex justify-content-end align-items-center">
                         <Button variant="outline-secondary">
+                            <div> Players</div>
                             <Image src="./assets/pug.svg" fluid />
                             <Badge variant="primary" className="mx-1">{props.players.length}</Badge>
                         </Button>
