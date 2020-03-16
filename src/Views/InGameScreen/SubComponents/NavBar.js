730db1406502026326fbd87d5703940a7e8f317d
Quang Luong <quanglddev@gmail.com>
2020-03-16 13:17:37 -0400

    No Text

--- src/Views/InGameScreen/SubComponents/NavBar.js
+++ src/Views/InGameScreen/SubComponents/NavBar.js
@@ -5,7 +5,7 @@ import Image from 'react-bootstrap/Image';
 import Badge from 'react-bootstrap/Badge';
 
 const NavBar = (props) => {
-    const playerNameItems = props.online_players.map((player,index) => {
+    const playerNameItems = props.online_players.map((player, index) => {
         if (player.name === props.current_player.name) {
             return (
                 <a className="p-2 font-weight-bold" href="#" key={index}>{player.name}: {player.points}</a>)
@@ -21,7 +21,7 @@ const NavBar = (props) => {
                 <div className="row flex-nowrap justify-content-between align-items-center">
                     <div className="col-4 pt-1">
                         <Button variant="outline-secondary">
-                            <div>My score:</div>
+                            {/* <div>My score:</div> */}
                             <Image src="./assets/crown.svg" fluid />
                             <Badge variant="warning" className="mx-1">{props.points}</Badge>
                         </Button>
@@ -31,7 +31,7 @@ const NavBar = (props) => {
                     </div>
                     <div className="col-4 pt-1 d-flex justify-content-end align-items-center">
                         <Button variant="outline-secondary">
-                            <div> Players</div>
+                            {/* <div> Players</div> */}
                             <Image src="./assets/pug.svg" fluid />
                             <Badge variant="primary" className="mx-1">{props.online_players.length}</Badge>
                         </Button>
