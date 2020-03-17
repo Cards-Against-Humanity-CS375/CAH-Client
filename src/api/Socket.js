0da5da061e6231ff936331ec37fdd97f2dca97a1
Quang Luong <quanglddev@gmail.com>
2020-03-17 14:52:26 -0400

    Production

--- src/api/Socket.js
+++ src/api/Socket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
-// const endpoint = 'https://fast-sands-73377.herokuapp.com'
-const endpoint = 'http://localhost:3002'
+const endpoint = 'https://fast-sands-73377.herokuapp.com'
+// const endpoint = 'http://localhost:3002'
 
 const socket = socketIOClient(endpoint)
 
